198974323c12f75dcb4bcb52d6e617a11dd802ec
kienvu15 <147035273+kienvu15@users.noreply.github.com>
2025-03-13 13:41:03 +0700

    F1

---
+++ Assets/Scrpits/LockDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LockDoor : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Hàm chạy animation
+    public void PlayAnimation()
+    {
+        if (anim != null)
+        {
+            anim.Play("LockDoorDisappear"); // Đảm bảo animation có parameter trigger "Open"
+        }
+        else
+        {
+            Debug.LogWarning("Animator chưa được gán trên object này!");
+        }
+    }
+    public void DoneAnim()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/SoftBlock.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SoftBlock : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private Vector3 initialPosition; // Lưu vị trí ban đầu
+    private bool isActivated = false;
+
+    [SerializeField] private float fallDelay = 1f;
+    [SerializeField] private float destroyDelay = 2f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        initialPosition = transform.position; // Lưu vị trí ban đầu
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isActivated = true;
+            anim.Play("Zinggel_Sofr");
+        }
+    }
+
+    public void DeActive()
+    {
+        boxCollider.enabled = false;
+    }
+
+    private void On2Destroy()
+    {
+        gameObject.SetActive(false); // Ẩn thay vì destroy
+    }
+
+    // Hàm Reset Platform
+    public void ResetPlatform()
+    {
+        gameObject.SetActive(true);
+        transform.position = initialPosition;
+        boxCollider.enabled = true;
+        isActivated = false;
+        anim.Play("Idle"); // Hoặc trạng thái mặc định
+    }
+}
