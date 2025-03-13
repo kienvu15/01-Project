e4a8bc3564dd4b4d3685eb5eb17b6dfb43a45ba6
kienvu15 <147035273+kienvu15@users.noreply.github.com>
2025-03-13 11:23:18 +0700

    Chain, Button, box

--- Assets/PressureButton.cs
+++ Assets/PressureButton.cs
@@ -1,16 +1,35 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PressureButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator anim;
+    private int objectCount = 0; // Đếm số object đang chạm vào nút
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (!collision.CompareTag("Untagged")) // Tránh kích hoạt bởi các object không mong muốn
+        {
+            objectCount++;
+            anim.Play("ButtonPress"); // Chạy animation nhấn nút
+            Debug.Log("🔴 Nút bị ấn xuống!");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Untagged"))
+        {
+            objectCount--;
+            if (objectCount <= 0) // Khi tất cả object rời đi
+            {
+                anim.Play("Button"); // Quay về trạng thái ban đầu
+                Debug.Log("🔵 Nút nhả ra!");
+            }
+        }
     }
 }
