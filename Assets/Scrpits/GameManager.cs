78f93e98fa3de89fe60d8433356527e3789a1c57
kienvu15 <147035273+kienvu15@users.noreply.github.com>
2025-03-19 16:09:05 +0700

    GOI

--- Assets/Scrpits/GameManager.cs
+++ Assets/Scrpits/GameManager.cs
@@ -1,10 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public FlashEffect flashEffect;
+    public GridSpawner gridSpawner;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level Select");
+        StartCoroutine(TeleportAfterFlash());
+        SceneManager.LoadScene("Stagelevel");
+    }
+
+    private IEnumerator TeleportAfterFlash()
+    {
+        yield return StartCoroutine(flashEffect.StartFlash());   
+        StartCoroutine(gridSpawner.SpawnGrid());
     }
 }
