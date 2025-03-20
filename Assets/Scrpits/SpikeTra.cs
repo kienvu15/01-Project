3591a1a2a9545de6dd939d35b1f170b8801187e2
xuandai0229 <daicongtu2910@gmail.com>
2025-03-21 00:33:26 +0700

    Capnhap

---
+++ Assets/Scrpits/SpikeTra.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) // Nếu người chơi chạm vào bẫy
+        {
+            Debug.Log("Player chạm vào vùng bẫy! Game Over!");
+            Time.timeScale = 0; // Dừng game
+        }
+    }
+}
