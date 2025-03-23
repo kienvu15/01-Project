1febf81de3fbdd89f2029ee362872022a4e4125a
kienvu15 <147035273+kienvu15@users.noreply.github.com>
2025-03-23 07:08:17 +0700

    update win scene

---
+++ Assets/WinGameUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class WinGameUI : MonoBehaviour
+{
+    public TextMeshProUGUI finishText;
+
+    void Start()
+    {
+        int totalDeaths = PlayerPrefs.GetInt("DeathCount", 0);
+        finishText.text = $"Die: {totalDeaths}";
+
+        // Reset số lần chết khi bấm nút bất kỳ
+        StartCoroutine(WaitForInput());
+    }
+
+    IEnumerator WaitForInput()
+    {
+        yield return new WaitUntil(() => Input.anyKeyDown);
+        ResetGame();
+    }
+
+    public void ResetGame()
+    {
+        PlayerPrefs.DeleteKey("DeathCount"); // Xóa số lần chết
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(0); // Quay về màn hình menu hoặc màn 1
+    }
+
+    //IEnumerator
+
+       
+}
